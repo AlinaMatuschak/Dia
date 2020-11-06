787c34ce4da47645c08f3cc2371b2e2b758cc796
Alina <alina90565@gmail.com>
2020-11-06 14:46:06 +0200

    refactor code

---
+++ src/scripts/buttonsLogic.js
@@ -0,0 +1,22 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+  document.location.reload();
+  event.preventDefault();
+});
+
+const buttonsLearnMore = document.querySelectorAll('.learn-more');
+
+buttonsLearnMore.forEach(button => {
+  button.addEventListener('click', event => {
+    document.location.href = '#about-us';
+    event.preventDefault();
+  });
+});
+
+const buttonApply = document.querySelector('.button-apply');
+
+buttonApply.addEventListener('click', event => {
+  document.location.href = '#contact-us';
+  event.preventDefault();
+});
